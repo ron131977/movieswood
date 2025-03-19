44839359456282045300ecd72783e7857a9961c1
ron131977 <ronalddsouza888@gmail.com>
2025-03-19 10:51:32 +0530

    yes

--- components/multi-source-player.tsx
+++ components/multi-source-player.tsx
@@ -55,7 +55,7 @@ export default function MultiSourcePlayer({ sources, title }: MultiSourcePlayerP
                 className="w-full h-full border-0"
                 allowFullScreen
                 onLoad={() => setIsLoading(false)}
-                style={{ filter: "contrast(1.2) saturate(1.3) brightness(1.0) hue-rotate(0deg)"}}
+                style={{ filter: "contrast(1.2) saturate(1.3) brightness(1.0) hue-rotate(10deg)"}}
               ></iframe>
             </div>
           </TabsContent>
