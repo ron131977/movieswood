e26e809f3dd3503da1d29ca273ff8dec2bd79a98
ron131977 <ronalddsouza888@gmail.com>
2025-03-19 21:33:26 +0530

    god do it

--- app/page.tsx
+++ app/page.tsx
@@ -237,7 +237,7 @@ export default async function Home() {
           </div>
         </div>
       </section>
-      <div className="flex justify-center items-center">
+      <div className="flex justify-center items-center" >
         <Search />
       </div>
       {/* Content Sections */}
